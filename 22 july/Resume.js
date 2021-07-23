9fc73d11835a9582e1398ea1dcf89779adaf193a
AKRaavanan <87131578+AKRaavanan@users.noreply.github.com>
2021-07-23 08:47:51 +0530

    Add files via upload

---
+++ 22 july/Resume.js
@@ -0,0 +1,17 @@
+var resume =[
+      {
+            Name:"Dinesh.R", address:'177/9 dhanakodi nayakar street,Madurai Road,trichy-620008',phone_num:"8190952887_8072681309",Email:"kingdinesh306@gmail.com",
+            Qualification:{UG:"BE-65%-Mechanical engg Annai mathammal sheela_Engg_clg",
+                          HSC:"70.3%-St.josephs Higher Secondary school", 
+                          SSLC: "80.3%-St.josephs Higher Secondary school"
+                          },
+            Additional_Skills:{CS:"PGDCA-90% (C, C++ Programming , Photoshop and Microsoft office)"},
+            Experience1:{ Role:"Development Junior Engineer,(May 2019-November 2020),Veejei Automation, Coimbatore",
+                Responsibilities:"Marking of Casting,Dimensional Inspection,ISO Related documentation,Visual inspection, Customer Verification Documentation, Dye Penetrant testing. Providing a proven method for production"
+                        },  
+            Experience2:{Role:"Fetling Supervisor(June 2018-May 2019),Eltex Super Casting Pvt Ltd, Coimbatore",
+             Responsibilities:"Fetling of casting,Stock Calculation,Ontime Production"},
+            Project:"Design of helical coil suspension by composite Material .The main focus of the work to evaluate and study the static structural analysis of open coiled helical spring. The selected materials for the analysis are AMC225xe, AMC640xa, Steel, Carbon fiber and their combinations. After theoretical calculation and analysis the combination of Steel and Carbon fiber having the better values in stiffness, deformation and shear stress",            
+            languages_known:"English,Tamil"
+      }] 
+      console.log(resume[0])
